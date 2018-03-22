702c7cec7f56dd33eda63467ba1a46cea24cce93
Naren SS <ufo2mstar@gmail.com>
2018-03-22 11:53:42 -0400

    POST: kod

--- jpush.bat
+++ jpush.bat
@@ -1,6 +1,20 @@
-cd ..\site\
-read -p "Commit desc: " desc
-git add . && \
-git add -u && \
-git commit -m "$desc" && \
-git push origin source
+:: cd ..\site\
+:: read -p "Commit desc: " desc
+:: git add . && \
+:: git add -u && \
+:: git commit -m "$desc" && \
+:: git push origin source
+@echo off
+echo Building project...
+jkbuild &
+echo Built locally... &
+cd ..\site\ &
+set /p msg="Please enter your site commit msg : "
+:: echo %msg%
+
+git add . &
+git add -u &
+git commit -m "POST: %msg%" &
+echo push done! &
+git push origin master &
+echo site is live @ http://ufo2mstar.github.io/
