436c806533dec37691f83bf3680bbb69e9ad44f4
ufo2mstar <ufo2mstar@gmail.com>
2018-03-22 12:02:23 -0400

    POST: mm bold

--- jpush.bat
+++ jpush.bat
@@ -5,9 +5,9 @@
 :: git commit -m "$desc" && \
 :: git push origin source
 @echo off
-echo Building project... &
-bundle exec jekyll build -d ..\site\ &
-echo Built locally... &
+::echo Building project... &
+::bundle exec jekyll build -d ..\site\ &
+::echo Built locally... &
 cd ..\site\ &
 set /p msg="Please enter your site commit msg : "
 :: echo %msg%
@@ -17,5 +17,6 @@ git add -u &
 git commit -m "POST: %msg%" &
 echo push done! &
 git push origin master &
+echo ... &
 echo site is live @ http://ufo2mstar.github.io/
 cd ..\merger
