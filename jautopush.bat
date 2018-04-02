644330551acec82ad63bb955a2fdc4ccfd44dceb
Naren SS <ufo2mstar@gmail.com>
2018-04-02 00:03:23 -0400

    RES: 2018 updates

---
+++ jautopush.bat
@@ -0,0 +1,22 @@
+:: cd ..\site\
+:: read -p "Commit desc: " desc
+:: git add . && \
+:: git add -u && \
+:: git commit -m "$desc" && \
+:: git push origin source
+@echo off
+::echo Building project... &
+::bundle exec jekyll build -d ..\site\ &
+::echo Built locally... &
+cd ..\site\ &
+::set /p msg="Please enter your site commit msg : "
+:: echo %msg%
+
+git add . &
+git add -u &
+git commit -m "POST: autopush" &
+echo commit done! &
+git push origin master &
+echo ... &
+echo site is live @ http://ufo2mstar.github.io/
+cd ..\merger
